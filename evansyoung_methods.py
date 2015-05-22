f099b9dc2d00dd5f48fc1e2e36104d88bee511d5
Sunwoo Kim <kim392@illinois.edu>
2015-05-22 17:02:48 -0500

    changed name

---
+++ evansyoung_methods.py
@@ -0,0 +1,102 @@
+import gdal
+from gdalconst import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as ml
+import matplotlib.cm as cm
+
+filename = "output_be.tif"
+dataset = gdal.Open(filename, GA_ReadOnly)
+
+cols = dataset.RasterXSize
+rows = dataset.RasterYSize
+bands = dataset.RasterCount
+driver = dataset.GetDriver().LongName
+
+geotransform = dataset.GetGeoTransform()
+originX = geotransform[0] # top left x
+originY = geotransform[3] # top left y
+pixelWidth = geotransform[1]
+pixelHeight= geotransform[5]
+
+band = dataset.GetRasterBand(1)
+data = band.ReadAsArray(0,0,cols,rows).astype(np.float)
+#rows = len(data) and cols = len(data[0])
+# Bounds for x: 0 to rows - 1
+# Bounds for y: 0 to cols - 1
+
+#-----------Terrain Gradients Evens-Young Method---------------
+
+def isOutOfBounds(x,y,rows,cols):
+	if( (x-1 < 0) or (x+1 > rows-1) or (y-1 < 0) or (y+1 > cols-1)):
+		return True
+	else:
+		return False
+
+def Z(x,y,n):
+	if n == 1:
+		return data[x-1,y-1]
+	elif n == 2:
+		return data[x,y-1]
+	elif n == 3:
+		return data[x+1,y-1]
+	elif n == 4:
+		return data[x-1,y]
+	elif n == 5:
+		return data[x,y]
+	elif n == 6:
+		return data[x+1,y]
+	elif n == 7:
+		return data[x-1, y+1]
+	elif n == 8:
+		return data[x, y+1]
+	elif n == 9:
+		return data[x+1,y+1]
+
+#G: First Derivative in x direction
+def getG(x,y,p):
+		return ( Z(x,y,3) + Z(x,y,6) + Z(x,y,9) - Z(x,y,1) - Z(x,y,4) - Z(x,y,7)) / (6 * p)
+
+#H: First Derivative in y direction
+def getH(x,y,p):	
+		return ( Z(x,y,1) + Z(x,y,2) + Z(x,y,3) - Z(x,y,7) - Z(x,y,8) - Z(x,y,9)) / (6 * p)
+
+#D: Second Derivative in x direction
+def getD(x,y,p):
+	return ( Z(x,y,1) + Z(x,y,3) + Z(x,y,4) - Z(x,y,6) - Z(x,y,7) - Z(x,y,9) - 2*(Z(x,y,2) + Z(x,y,5) + Z(x,y,8)) ) / (3 * p*p) 
+
+#E: Second Derivative in y direction
+def getE(x,y,p):
+	return ( Z(x,y,1) + Z(x,y,2) + Z(x,y,3) - Z(x,y,7) - Z(x,y,8) - Z(x,y,9) - 2*(Z(x,y,4) + Z(x,y,5) + Z(x,y,6)) ) / (3 * p*p)
+
+#F: Second derivative along diagonals
+def getF(x,y,p):
+	return ( Z(x,y,3) + Z(x,y,7) - Z(x,y,1) - Z(x,y,9) ) / (4 * p*p)
+
+# plot function for better visualization
+def plot(data):
+    fig = plt.figure()
+    plt.imshow(data)
+    plt.colorbar(orientation='vertical')
+    plt.show()
+    
+	
+def main():
+	G = H = D = E = F = np.zeros((rows, cols))
+	p = pixelWidth
+   	for i in range(data.shape[0]):
+		for j in range(data.shape[1]):
+			if (not isOutOfBounds(i,j,rows,cols)):
+				G[i,j] = getG(i,j,p)
+				H[i,j] = getH(i,j,p)
+				D[i,j] = getD(i,j,p)
+				E[i,j] = getE(i,j,p)
+				F[i,j] = getF(i,j,p)
+	#Commented out the plots due to RuntimeError: Invalid DISPLAY variable on roger
+	#plot(G)
+	#plot(H)
+	#plot(D)
+	#plot(E)
+	#plot(F)
+
+main()
