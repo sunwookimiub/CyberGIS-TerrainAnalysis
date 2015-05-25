69d13c8b49aca5c5ee8ce00a4ebae7ee7a676001
Sunwoo Kim <swk0205@gmail.com>
2015-05-25 12:59:09 -0500

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from mpi4py import MPI
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_rank()
-name = MPI.Get_processor_name()
-
-print("Rank:%d, Size:%d, Name:%s" % (rank,size,name))
