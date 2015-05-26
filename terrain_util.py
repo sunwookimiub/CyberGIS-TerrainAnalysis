84afed7672cabcd3c7b82f20665c01a4f62d67c2
Sunwoo Kim <swk0205@gmail.com>
2015-05-26 10:07:26 -0500

    Delete terrain_util.py

--- terrain_util.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-def parseArgs():
-	parser = argparse.ArgumentParser(description='Process from geoTIFF')
-	parser.add_argument('filename', metavar='T', type=str, nargs='+', help='a geoTIFF file for analysis')
-	args = parser.parse_args()
-	print args.accumulate(args.integers)
-	
