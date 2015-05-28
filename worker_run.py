a441f55317a0937bd46aa24e932ea90a594628fb
Sunwoo Kim <kim392@illinois.edu>
2015-05-28 10:51:25 -0500

    changed comments

--- worker_run.py
+++ worker_run.py
@@ -5,29 +5,31 @@ from worker_util import *
 from mpi4py import MPI
 from gdalconst import *
 
-# this function assign roughly equally devided data to each process, then each process do the computation independently.
+# The function run_mpi_jobs assigns equally divided data to each process
+# Each process performs the computation independently
 def run_mpi_jobs (file, p, output_file):
 	comm = MPI.COMM_WORLD
 	rank = comm.Get_rank()
 	size = comm.Get_size()
 
-        # output is in geo tiff format
+    # Current output format is geoTIFF
 	driver = gdal.GetDriverByName("GTiff")	
 	dataset = gdal.Open(file, GA_ReadOnly)
 	band = dataset.GetRasterBand(1)
 	geotransform = dataset.GetGeoTransform()
-        # if using default pixel size, get pixel size from data 
+    # if using default pixel size, get pixel size from data 
 	if p == 0:
 		p = geotransform[1] 
 	cols = dataset.RasterXSize
-        # number of columns for each process
+    # number of columns for each process
 	proc_cols = cols/size
-        # each process is assigned with same number of columns as original data
+    # each process is assigned with same number of columns as original data
 	y_size = dataset.RasterYSize
 	
-        # the process with highest rank get the last chunk of data 
+    # The process with highest rank receives the last chunk of data 
 	if rank == size-1 :
-                # in order to process boundaries get one more column of data from left neighbor 
+		# Process the boundaries
+        # One more column of data is attained from the neighbor to the left 
 		x_offset = rank*proc_cols-1
 		proc_cols = cols - proc_cols * (size-1)
 		G = np.zeros((y_size, proc_cols))
@@ -35,9 +37,10 @@ def run_mpi_jobs (file, p, output_file):
 
 		process_bands(band, p, x_offset, x_size, y_size, G)
 
-        # process with lowest rank get the first chunk of data
+        # The process with lowest rank receives the first chunk of data
 	elif rank == 0:
-                # in order to process boundaries, get one more column of data from right neighbor 
+		# Again, process the boundaries
+		# One more column of data is attained from the neighbor to the right
 		G = np.zeros((y_size, proc_cols))
 		x_offset = 0
 		x_size = proc_cols + 1
@@ -46,22 +49,24 @@ def run_mpi_jobs (file, p, output_file):
         
 
 	else:
-                # get two more columns of data from neighbors to process boundaries
+        # Again, process the boundaries
+		# Two more columns of data is attained from neighbors 
 		G = np.zeros((y_size, proc_cols))
 		x_offset = rank*proc_cols-1
 		x_size = proc_cols+2
 		
 		process_bands(band, p, x_offset, x_size, y_size, G)
                 
-        # wait for all processes finish processing
+    # Barrier is called to wait for all processes 
 	comm.Barrier()
-        # rank 0 gathers all processed data
+    # The root, rank 0, gathers all processed data
 	data = comm.gather(G, root=0)
 	
 	if rank == 0:
-                # output processed data
+        # output processed data
 		data = np.concatenate(data, axis=1)
-		output_dataset = driver.Create(output_file, cols, y_size, 1, gdal.GDT_Float32)
+		output_dataset = driver.Create(output_file, cols, y_size, 1, \
+							gdal.GDT_Float32)
 		output_dataset.SetGeoTransform(geotransform)
 		output_dataset.GetRasterBand(1).WriteArray(data)
 		output_dataset = None
