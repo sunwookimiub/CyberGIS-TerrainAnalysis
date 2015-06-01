a97a4cb68f66827527c905e956fcef774fe32c4a
Sunwoo Kim <swk0205@gmail.com>
2015-06-01 17:53:50 -0500

    Update worker_util.py

--- worker_util.py
+++ worker_util.py
@@ -20,72 +20,57 @@ def process_bands(band, p, x_offset, x_size, y_size):
     # Set how RuntimeWarning: invalid value encountered in divide
     # error is handled. This error derives from division by Nan
     np.seterr(divide='ignore', invalid='ignore')
-    # The assigned block of image data to be processed
-    proc_data = band.ReadAsArray(x_offset,0,x_size,y_size)
-    # Create a numpy array of offsetted size
+    proc_data = band.ReadAsArray(x_offset, 0, x_size, y_size)
     output_data = np.zeros((10, y_size-2, x_size-2))
 
     # Implementation of Terrain Gradients Evans-Young method
-    # G first derivative in x direction
-    output_data[0] = G =getG(proc_data, p)
-    # H first derivative in y direction
+    output_data[0] = G = getG(proc_data, p)
     output_data[1] = H = getH(proc_data, p)
-    # D second derivative in x direction
     output_data[2] = D = getD(proc_data, p)
-    # E second derivative in y direction
-    output_data[3] = E = getE(proc_data,p)
-    # F second derivative along diagonals
+    output_data[3] = E = getE(proc_data, p)
     output_data[4] = F = getF(proc_data, p)
 
     # Implemented Morphometrkic Parameters
-    # Commonly used variables
-    # G*H*F
     GHF = np.multiply(F, np.multiply(G, H) )
-    # G^2 + H^2
     G2H2 = np.power(G, 2) + np.power(H, 2)
-    # Slope
     output_data[5] = np.sqrt(G2H2)
-    # Aspect
     output_data[6] = np.arctan(H/G)
-    # Planc
-    output_data[7] = - ( ( np.multiply(np.power(H,2),D)  \
-        - 2 * GHF + np.multiply(np.power(G, 2), E) ) / np.power(G2H2, 1.5) )
-    # Profc
-    output_data[8] = - ( (np.multiply(np.power(G, 2), D) + 2 * GHF \
-        + np.multiply(np.power(H,2), E) ) \
-        / (np.multiply(G2H2, np.power(1+G2H2,1.5) ) ) )
-    # Meanc
-    output_data[9] = - ( (np.multiply(1 + np.power(H,2), D) - 2 * GHF \
-        + np.multiply(np.power(G,2), E) ) / (2 * np.power(1 + G2H2, 1.5) ) )
+    output_data[7] = - (np.multiply(np.power(H, 2), D)  \
+        - 2*GHF + np.multiply(np.power(G, 2), E)) / np.power(G2H2, 1.5)
+    output_data[8] = - (np.multiply(np.power(G, 2), D) + 2*GHF \
+        + np.multiply(np.power(H, 2), E)) \
+        / (np.multiply(G2H2, np.power(1+G2H2, 1.5)))
+    output_data[9] = - (np.multiply(1+np.power(H, 2), D) - 2*GHF \
+        + np.multiply(np.power(G, 2), E)) / (2*np.power(1+G2H2, 1.5)) 
 
     return output_data
 
 def getG(data, p):
-    # Return G, first derivative in x direction
+    """Return G, first derivative in x direction"""
     bar  = get_block(data)
-    return( (bar[2]+bar[5]+bar[8]-bar[0]-bar[3]-bar[6]) / (6*p) )
+    return (bar[2]+bar[5]+bar[8]-bar[0]-bar[3]-bar[6]) / (6*p)
 
 def getH(data, p):
-    # Return H, first derivative in y direction
+    """Return H, first derivative in y direction"""
     bar = get_block(data)
-    return( (bar[0]+bar[1]+bar[2]-bar[6]-bar[7]-bar[8] ) / (6*p) )
+    return (bar[0]+bar[1]+bar[2]-bar[6]-bar[7]-bar[8]) / (6*p)
 
 def getD(data, p):
-    # Return D, second derivative in x direction.
+    """Return D, second derivative in x direction"""
     bar = get_block(data)
-    return( (bar[0]+bar[2]+bar[3]-bar[5]-bar[6]-bar[8] \
-                -2*(bar[1]+bar[4]+bar[7]) ) /(3*p*p) )
+    return (bar[0]+bar[2]+bar[3]-bar[5]-bar[6]-bar[8] \
+        - 2*(bar[1]+bar[4]+bar[7])) / (3*p*p)
 
 def getE(data, p):
-    # Return E, second derivative in x direction.
+    """Return E, second derivative in x direction"""
     bar = get_block(data)
-    return( (bar[0]+bar[1]+bar[2]-bar[6]-bar[7]-bar[8] \
-            - 2*(bar[4]+bar[5]+bar[6]) ) / (3*p*p) )
+    return (bar[0]+bar[1]+bar[2]-bar[6]-bar[7]-bar[8] \
+        - 2*(bar[4]+bar[5]+bar[6])) / (3*p*p)
 
 def getF(data, p):
-    # Return F, second derivative along diagonals.
+    """Return F, second derivative along diagonals"""
     bar = get_block(data)
-    return( (bar[2]+bar[6]-bar[0]-bar[8] ) / (4*p*p) )
+    return (bar[2]+bar[6]-bar[0]-bar[8]) / (4*p*p)
 
 def get_block(data):
     """return array with different slices to ease access to nearest neighbors
