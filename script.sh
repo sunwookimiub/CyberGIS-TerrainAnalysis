aac3fd5687524f62cf264ef6dbeea2504ba9a02d
Sunwoo Kim <kim392@illinois.edu>
2015-06-01 11:50:32 -0500

    parsing

--- script.sh
+++ script.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env sh
-#PBS -N output_qsub
+#PBS -N qsub_output
 #PBS -j oe
 
 cd $PBS_O_WORKDIR
 chmod +x terrain_main.py
 module load anaconda
-/usr/bin/mpirun -np $PBS_NP python terrain_main.py $inImg $outImg $p
+
+outImg_="output_image.tif"
+p_=0
+
+if [ -n "$inImg" ]; then
+
+    if ! [ -e $inImg ]; then
+        echo "$inImg file does not exist."
+    fi
+
+    if [ -n "$outImg" ]; then outImg_=$outImg
+    fi
+
+    if [ -n "$p" ]; then p_=$p
+    fi
+    
+else echo "Please input an input image file."
+fi
+
+/usr/bin/mpirun -np $PBS_NP python terrain_main.py $inImg $outImg_ $p_
--- terrain_main.py
+++ terrain_main.py
@@ -1,11 +1,27 @@
-import sys
+import argparse
+import textwrap
 from worker_run import *
 
-def main():
-	file = sys.argv[1]
-	output=sys.argv[2]
-	p = float(sys.argv[3])
-	run_mpi_jobs(file, p, output)
-	
+def parseArguments():
+    parser = argparse.ArgumentParser( \
+        usage = 'qsub script.sh -v inImg=a[,outImg=b,p=c] [-l nodes=x:ppn=y]',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent('''\
+                        Program Description
+                        -----------------------------------------
+                        To receive an image file as input
+                        from the user and to return a file of the
+                        equivalent format with values calculated
+                        by running the Evans-Young methods in
+                        parallel.
+            '''))
+
+    parser.add_argument("inImg", type=str, help="Name of the file need to be analysed")
+    parser.add_argument("outImg", type=str, help="Name of the output file. Default value: output_image.tif")
+    parser.add_argument("p", type=int, help="Pixel size. Default value: pixel size of the given input image")
+    parser.add_argument("-l", help= "nodes=Number of Nodes:ppn=Number of ppn")
+    return parser.parse_args()
+
 if __name__ == ("__main__") :
-	main()
+    args = parseArguments()
+    run_mpi_jobs(args.inImg, args.p, args.outImg)
