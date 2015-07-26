55edaac8247a03d7839d0413e7d1426b124eae20
kim392 <kim392@illinois.edu>
2015-07-26 17:12:10 -0500

    wr wu from bitbucket

--- worker_run.py
+++ worker_run.py
@@ -1,11 +1,11 @@
+import sys
 import gdal
 import time
 import numpy as np
+import logging
 from worker_util import *
 from mpi4py import MPI
 from gdalconst import *
-
-
 
 def write_to_file(data, x_size, y_size, 
                   output_file_name, input_driver_name):
@@ -25,56 +25,169 @@ def write_to_file(data, x_size, y_size,
         output_dataset.GetRasterBand(i+1).WriteArray(data[i], 1, 1)
     output_dataset = None
 
-def run_mpi_jobs (file, p, output):
+def create_raster(x_size, y_size, output_file_name, 
+                  input_driver_name, georef, proj):
+    """Create the output raster file
+    
+    Keyword argument:
+    x_size              -- size in x dimension for output raster band
+    y_size              -- size in y dimension for output raster band
+    output_file_name    -- file name for output data
+    input_driver_name   -- output file format
+    georef              -- geo reference from input data
+    proj                -- projection from input data
+
+    Return value:
+    output_dataset      -- output data set 
+    """
+    driver = gdal.GetDriverByName(input_driver_name)
+    output_dataset = driver.Create(output_file_name, x_size, 
+                                   y_size, 10, gdal.GDT_Float32)
+    output_dataset.SetGeoTransform(georef)
+    output_dataset.SetProjection(proj)
+    return output_dataset
+        
+    
+def write_raster(dataset, data, x_offset, 
+                 y_offset, nodata):
+    """ Write data to 10 bands of output file 
+            
+    Keyword arguments:
+    dataset             -- output data set
+    data                -- data to be write, numpy array 
+    x_offset            -- x offset for each band 
+    y_offset            -- y offset for each band
+    nodata              -- nodata value for each band
+    """
+    for i in range(data.shape[0]):
+        dataset.GetRasterBand(i+1).WriteArray(data[i], x_offset, y_offset)
+        dataset.GetRasterBand(i+1).SetNoDataValue(nodata)
+    
+
+def run_mpi_jobs (file, output, nodata, band_index):
     """ Assign roughly equally divided data chunk to each process
 
     Keyword argument:
     file   -- input file name
-    p      -- pixel size
     output -- output file name
+    nodata -- nodata value for missing data points
+    band   -- input raster band
     """
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
+    
     dataset = gdal.Open(file, GA_ReadOnly)
-    input_driver_name = dataset.GetDriver().ShortName
-    band = dataset.GetRasterBand(1)
+    input_driver_name = dataset.GetDriver().ShortName # get file format for output
+    if not band_index:
+        band_index = 1
+    band = dataset.GetRasterBand(band_index)
+    
+    if not nodata:
+        nodata = band.GetNoDataValue()
+
     geotransform = dataset.GetGeoTransform()
-    # if using default pixel size, get pixel size from data
-    p = geotransform[1]
+    proj = dataset.GetProjection()
+    
     cols = dataset.RasterXSize
     rows = dataset.RasterYSize
-    # roughly assign equal number of rows to each process
-    proc_rows = rows/size
-    # each process is assigned with same number of columns as original data
-    x_size = dataset.RasterXSize
 
-    # the process with highest rank get the last chunk of data
-    if rank == size - 1:
-        # in order to process boundaries get one more row of data from left neighbor
+    # compute corner coordinates
+    minx = geotransform[0]
+    miny = geotransform[3] + cols*geotransform[4] + rows*geotransform[5]
+    maxx = geotransform[0] + cols*geotransform[1] + rows*geotransform[2]
+    maxy = geotransform[3]
+
+    # compute pixel sizes 
+    px, py = get_pixel(proj, geotransform) 
+    
+    # print useful geo infomation
+    if rank == 0:
+        logging.info("Coordinate system is: %s", dataset.GetProjection())
+        logging.info("Corner Coordinates:")
+        logging.info("Upper left: (%f, %f)", minx, maxy)
+        logging.info("Upper right: (%f, %f)", maxx, maxy)
+        logging.info("Lower left: (%f, %f)", minx, miny)
+        logging.info("Upper right: (%f, %f)", maxx, miny)
+        logging.info("nodata is: %g", nodata)
+        logging.info("pixel size = (%f, %f)", px, py)
+
+    # data required for process
+    proc_rows = rows/size
+    x_size = dataset.RasterXSize
+    y_size = 0
+    x_offset = 0
+    y_offset = 0
+
+    if rank == 0:
+        t1 = time.time()
+
+    
+    if size == 1 :
+        y_offset = 0
+        y_size = proc_rows
+        output_data = calculate_operations(band, px, py, y_offset, x_size, y_size)
+        
+    elif rank == size - 1:
+        # to process boundaries get one more row of data from left neighbor
         y_offset = rank*proc_rows - 1
         y_size = rows - proc_rows*(size-1) + 1
-        output_data = process_bands(band, p, y_offset, x_size, y_size)
+        output_data = calculate_operations(band, px, py, y_offset, x_size, y_size)
 
-        # process with lowest rank get the first chunk of data
     elif rank == 0:
-        # in order to process boundaries, get one more row of data from right neighbor
+        # to process boundaries, get one more row of data from right neighbor
         y_offset = 0
         y_size = proc_rows + 1
-        output_data = process_bands(band, p, y_offset, x_size, y_size)
-    else:
-        # get two more rows of data from neighbors to process boundaries
-        y_offset = rank*proc_rows - 1
-        y_size = proc_rows + 2
-        output_data = process_bands(band, p, y_offset, x_size, y_size)
+        output_data = calculate_operations(band, px, py, y_offset, x_size, y_size)
 
-    # wait for all processes finish processing
+    # last worker gets get remainder of rows
+    elif rank == size - 1:
+        y_offset = rank * proc_rows - 1
+        y_size = rows - proc_rows * (size-1) + 1
+
+    # all other workers get 2 extra rows
+    else:
+        y_offset = rank * proc_rows - 1
+        y_size = proc_rows + 2
+        output_data = calculate_operations(band, px, py, y_offset, x_size, y_size)
     comm.Barrier()
-    # close input dataset
+    
+    # close input data
     dataset = None
-    # rank 0 gathers all processed data
-    data = comm.gather(output_data, root=0)
+
+
+    # write output to file
+    # root proc create output file and writes iis output to file 
     if rank == 0:
-        # output processed data
-        data = np.concatenate(data, axis=1)
-        write_to_file(data, cols, rows, output, input_driver_name)
+        t2 = time.time()
+        output_dataset = create_raster(cols, rows, output, 
+            input_driver_name, geotransform, proj)
+        write_raster(output_dataset, output_data, 1, 1, nodata)
+
+
+    for proc in range(1, size):
+        # worker proc send output matrix size to root proc 
+        if rank == proc:
+            comm.send(output_data.shape, dest=0, tag=proc)
+        elif rank == 0:
+            proc_output_dim = comm.recv(source=proc, tag=proc)
+        comm.Barrier()
+        # worker proc send output data to root proc
+        if rank == proc:
+            comm.Send(output_data, dest=0, tag=proc)
+            logging.debug('process %s finishes sending data', str(proc))
+        elif rank == 0:            
+            buffered_output = np.zeros(proc_output_dim)
+            comm.Recv(buffered_output, source=proc, tag=proc)
+            # root proc write output to file
+            write_raster(output_dataset, buffered_output, 1, proc*proc_rows,
+                         nodata)
+        comm.Barrier()
+
+    # close output file 
+    output_dataset = None
+    
+    if rank == 0:
+        t_end = time.time()
+        logging.debug("write time: %f ", t_end-t2)
+        logging.debug("total time: %f ", t_end-t1)
--- worker_util.py
+++ worker_util.py
@@ -1,8 +1,14 @@
 import gdal
+import logging
 from gdalconst import *
 import numpy as np
+import math
+import time
+import sys
 
-def process_bands(band, p, y_offset, x_size, y_size):
+_MIN_ZERO_ONE = 1e-5
+THRESHOLD = 1e-6
+def calculate_operations(band, px, py,  y_offset, x_size, y_size):
     """Read and analysis elevation data.
     
     Keyword arguments:
@@ -20,29 +26,50 @@ def process_bands(band, p, y_offset, x_size, y_size):
     # Set how RuntimeWarning: invalid value encountered in divide
     # error is handled. This error derives from division by Nan
     np.seterr(divide='ignore', invalid='ignore')
+    t0 = time.time()
     proc_data = band.ReadAsArray(0, y_offset, x_size, y_size)
+    nodata = band.GetNoDataValue()
+    min_value = np.min(proc_data)
+    t1 = time.time()
     output_data = np.zeros((10, y_size-2, x_size-2))
+    if nodata:
+        nodata_ind = abs(proc_data - nodata)/abs(nodata)<THRESHOLD
+        np.place(proc_data, nodata_ind, float('nan')) # Implementation of Terrain Gradients Evans-Young method
+    output_data[0] = getG(proc_data, px) 
+    output_data[1] = getH(proc_data, py)
+    output_data[2] = getD(proc_data, px)
+    output_data[3] =  getE(proc_data, py)
+    output_data[4] = getF(proc_data, px, py)
 
-    # Implementation of Terrain Gradients Evans-Young method
-    output_data[0] = G = getG(proc_data, p)
-    output_data[1] = H = getH(proc_data, p)
-    output_data[2] = D = getD(proc_data, p)
-    output_data[3] = E = getE(proc_data, p)
-    output_data[4] = F = getF(proc_data, p)
-
+    for i in range(5):
+        np.place(output_data[i], abs(output_data[i]) < _MIN_ZERO_ONE, 0)
+    G = output_data[0]
+    H = output_data[1]
+    D = output_data[2]
+    E = output_data[3]
+    F = output_data[4]
     # Implemented Morphometrkic Parameters
     GHF = np.multiply(F, np.multiply(G, H) )
     G2H2 = np.power(G, 2) + np.power(H, 2)
     output_data[5] = np.sqrt(G2H2)
     output_data[6] = np.arctan(H/G)
     output_data[7] = - (np.multiply(np.power(H, 2), D)  \
-        - 2*GHF + np.multiply(np.power(G, 2), E)) / np.power(G2H2, 1.5)
+                            - 2*GHF + np.multiply(np.power(G, 2), E)) \
+                            / np.power(G2H2, 1.5)
     output_data[8] = - (np.multiply(np.power(G, 2), D) + 2*GHF \
-        + np.multiply(np.power(H, 2), E)) \
-        / (np.multiply(G2H2, np.power(1+G2H2, 1.5)))
+                            + np.multiply(np.power(H, 2), E)) \
+                            / (np.multiply(G2H2, np.power(1+G2H2, 1.5)))
     output_data[9] = - (np.multiply(1+np.power(H, 2), D) - 2*GHF \
-        + np.multiply(np.power(G, 2), E)) / (2*np.power(1+G2H2, 1.5)) 
-
+                            + np.multiply(np.power(G, 2), E)) / \
+                            (2*np.power(1+G2H2, 1.5)) 
+    for i in range(5, 10):
+        np.place(output_data[i], np.isnan(output_data[i]), 0)
+    # For nodata points in input data, change them back to nodata in output
+    for i in range(1, 10):
+        np.place(output_data[i], nodata_ind[1:-1, 1:-1], nodata)
+    t2 = time.time()
+    logging.debug("reading time %f", t1 - t0)
+    logging.debug("process time %f", t2 - t1)
     return output_data
 
 def getG(data, p):
@@ -58,19 +85,19 @@ def getH(data, p):
 def getD(data, p):
     """Return D, second derivative in x direction"""
     bar = get_block(data)
-    return (bar[0]+bar[2]+bar[3]-bar[5]-bar[6]-bar[8] \
-        - 2*(bar[1]+bar[4]+bar[7])) / (3*p*p)
+    return (bar[0]+bar[2]+bar[3]+bar[5]+bar[6]+bar[8] \
+            - 2*(bar[1]+bar[4]+bar[7])) / (3*p*p)
 
 def getE(data, p):
-    """Return E, second derivative in x direction"""
+    """Return E, second derivative in y direction"""
     bar = get_block(data)
-    return (bar[0]+bar[1]+bar[2]-bar[6]-bar[7]-bar[8] \
-        - 2*(bar[4]+bar[5]+bar[6])) / (3*p*p)
+    return (bar[0]+bar[1]+bar[2]+bar[6]+bar[7]+bar[8] \
+            - 2*(bar[4]+bar[5]+bar[6])) / (3*p*p)
 
-def getF(data, p):
+def getF(data, px, py):
     """Return F, second derivative along diagonals"""
     bar = get_block(data)
-    return (bar[2]+bar[6]-bar[0]-bar[8]) / (4*p*p)
+    return (bar[2]+bar[6]-bar[0]-bar[8]) / (4*px*py)
 
 def get_block(data):
     """return array with different slices to ease access to nearest neighbors
@@ -91,3 +118,49 @@ def get_block(data):
         data[1:-1,  :-2], data[1:-1, 1:-1], data[1:-1, 2:  ],
         data[2:  ,  :-2], data[2:  , 1:-1], data[2:  , 2:  ],
     ]
+
+def get_unit(str):
+    pos = str.find("UNIT")
+    if -1 == pos:
+        print "invalid string"
+        sys.exit()
+    pos = str.find('"', pos)
+    if -1 == pos:
+        print "invalid string"
+        sys.exit()
+    pos2 = str.find('"', pos+1)
+    unit = str[pos+1:pos2]
+    return unit
+
+def get_distance(lat1, lon1, lat2, lon2):
+    R = 6371000
+    theta1 = math.radians(lat1)
+    theta2 = math.radians(lat2)
+    delta = math.radians(lat2-lat1)
+    lamda = math.radians(lon2-lon1)
+    a = math.sin(delta/2) ** 2 + math.cos(theta1) * math.cos(theta2) *\
+            math.sin(lamda/2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = R*c
+    return d
+
+def get_pixel(projection, geotransform):
+    cordx = geotransform[0]
+    cordy = geotransform[3]
+    xpixel = geotransform[1]
+    ypixel = geotransform[5]
+    unit = get_unit(projection)
+    if unit == "degree":        #for lat/lon unit
+        xunit = get_distance(cordy, cordx, cordy, cordx+xpixel)
+        yunit = get_distance(cordy, cordx, cordy+ypixel, cordx)
+        return xunit, yunit
+    elif unit == "metre":       #for meter unit
+        return abs(xpixel), abs(ypixel)
+    elif unit == "feet" or unit == "Foot_US" or unit == "Degree":
+        factor = 0.3048006096012192
+        return factor*abs(xpixel), factor*abs(ypixel)
+    else:
+        print projection
+        print unit
+        print "Error: invalid unit for the input file"
+        sys.exit(-1)
