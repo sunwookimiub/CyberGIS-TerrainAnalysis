a970ed5fa2a85b43ee0c771cd919379adf9e4c53
kim392 <kim392@illinois.edu>
2015-07-26 17:18:26 -0500

    mv ta from bitbucket

--- terrain_analysis.py
+++ terrain_analysis.py
@@ -1,27 +1,52 @@
+#!/usr/bin/env python
+"""
+Reads a raster input file with elevation data and produces a multi-band
+raster output based on Evans-Young method.
+"""
 import argparse
-import textwrap
+import logging
+
 from worker_run import *
 
 def parseArguments():
-    parser = argparse.ArgumentParser( \
-        usage = 'qsub script.sh -v inImg=a[,outImg=b,p=c] [-l nodes=x:ppn=y]',
+    parser = argparse.ArgumentParser(
+        usage = 'mpirun -np <procs> %s [options] input output' % __file__,
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        description=textwrap.dedent('''\
-                        Program Description
-                        -----------------------------------------
-                        To receive an image file as input
-                        from the user and to return a file of the
-                        equivalent format with values calculated
-                        by running the Evans-Young methods in
-                        parallel.
-            '''))
+        description=__doc__)
 
-    parser.add_argument("inImg", type=str, help="Name of the file need to be analysed")
-    parser.add_argument("outImg_", type=str, help="Name of the output file. Default value: output_image.tif")
-    parser.add_argument("p_", type=int, help="Pixel size. Default value: pixel size of the given input image")
-    parser.add_argument("-l", help= "nodes=Number of Nodes:ppn=Number of ppn")
+    parser.add_argument("input", type=str, 
+        help="input raster file")
+    parser.add_argument("output", type=str, 
+        help="output raster file")
+    parser.add_argument("--nodata", type=float,
+        help="value for no data points (default=matches input file)")
+    parser.add_argument("--band", type=int, default=1,
+        help="input raster band (default=1)")
+    parser.add_argument("-v", "--verbose",  action='store_true', default=False,
+        help = "print progress related information")
+    parser.add_argument("-d", "--debug", action='store_true', default=False,
+        help = "print detailed debugging information")
+
     return parser.parse_args()
 
-if __name__ == "__main__" :
+
+def main():
     args = parseArguments()
-    run_mpi_jobs(args.inImg, args.p_, args.outImg_)
+
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO, 
+                format='%(asctime)s %(message)s',
+                datefmt='%I:%M:%S')
+    elif args.debug:
+        logging.basicConfig(level=logging.DEBUG, 
+                format='%(asctime)s %(message)s',
+                datefmt='%I:%M:%S')
+    else:
+        logging.basicConfig(format='%(asctime)s %(message)s',
+                datefmt='%I:%M:%S')
+
+    run_mpi_jobs(args.input, args.output, args.nodata, args.band)
+
+
+if __name__ == "__main__" :
+    main()
